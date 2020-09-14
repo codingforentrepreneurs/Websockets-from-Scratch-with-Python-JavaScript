63ce3dbfe98bcf40cada85a2ab36c908c9e813de
CFE <hello@teamcfe.com>
2020-09-14 11:49:00 -0700

    14 - React useState

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
 
+const Btn = () =>{
+    const [clickCount, setClickCount] = useState(0)
+
+    const handleClick = (event) =>{
+        // alert("btn in react")
+        setClickCount(clickCount + 1)
+    }
+    return <button onClick={handleClick} className={`class-${clickCount}`}>hello react btn</button>
+}
+
 const App = () => {
-    return <div>Hello world again.</div>
+    return <div>Hello world again.<Btn /></div>
 }
 
 const htmlElement = document.getElementById("app")
