51dbdd060fe2ebb996087f44fb36bbe31319111f
CFE <hello@teamcfe.com>
2020-09-14 12:26:10 -0700

    15 - The Basics of Props in React Components

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
 
-const Btn = () =>{
-    const [clickCount, setClickCount] = useState(0)
+const BtnLabel = (props) =>{
 
+    return <span> - {props.count} </span>
+}
+
+const Btn = (props) =>{
+    const [clickCount, setClickCount] = useState(0)
+    console.log(props)
+    const shouldRenderCount = props.renderCount === true ? true : false
+    const renderCountLabel = shouldRenderCount === true ? <BtnLabel count={clickCount} /> : null
     const handleClick = (event) =>{
         // alert("btn in react")
         setClickCount(clickCount + 1)
     }
-    return <button onClick={handleClick} className={`class-${clickCount}`}>hello react btn</button>
+    return <button onClick={handleClick} className={`class-${clickCount}`}>hello react btn {renderCountLabel}</button>
 }
 
 const App = () => {
-    return <div>Hello world again.<Btn /></div>
+    return <div>Hello world again.
+        
+       <p> <Btn renderCount /></p>
+       <p> <Btn /></p>
+       <p> <Btn /></p>
+       <p> <Btn /></p>
+    </div>
 }
 
 const htmlElement = document.getElementById("app")
